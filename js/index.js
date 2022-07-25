91e56c17eaf32489b87a95422da79fc48fdbbd59
Gema-Munoz <gema.m.moscoso@gmail.com>
2022-07-25 21:23:26 +0200

    js-iteraction1-2-3

--- js/index.js
+++ js/index.js
@@ -16,3 +16,14 @@ console.log(`It seems that the navigator has the longest name, it has ${hacker2.
 else if (hacker1.length === hacker2.length)
 console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 // Iteration 3: Loops
+let spaceHacker = ""
+for (i=0 ; i< hacker1.length ; i++){
+    spaceHacker+=hacker1[i].toUpperCase()+" "
+}
+console.log(spaceHacker)
+
+let reverseHacker = ""
+for (i=hacker2.length-1 ; i >= 0 ; i--){
+    reverseHacker+=hacker2[i]
+}
+console.log(reverseHacker)
