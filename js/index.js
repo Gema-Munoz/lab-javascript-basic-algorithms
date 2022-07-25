db85044633ac25b5f1781b541d113a99539d99e0
Gema-Munoz <gema.m.moscoso@gmail.com>
2022-07-25 20:37:04 +0200

    js-iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+console.log("I'm ready!");
+
+
 // Iteration 1: Names and Input
 //
+hacker1 = "Ren"
+console.log("The driver's name is "+hacker1);
+hacker2 = "Kaito"
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
