7f8767aad7933eb0a2e2d8e190701f77eadf8d82
Gema-Munoz <gema.m.moscoso@gmail.com>
2022-07-25 21:30:28 +0200

    js-iteraction1-2-3

--- js/index.js
+++ js/index.js
@@ -28,9 +28,9 @@ for (i=hacker2.length-1 ; i >= 0 ; i--){
 }
 console.log(reverseHacker)
 
-if (hacker1 > hacker2)
+if (hacker1 < hacker2)
  console.log(`The driver's name goes first.`)
-else if (hacker1 < hacker2)
+else if (hacker1 >  hacker2)
   console.log(`Yo, the navigator goes first definitely.`)
 else
 console.log("What!? You both have the same name?")
